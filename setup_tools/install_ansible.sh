5353f6a65e446652ee041f67204d52d7337ad8ca
naveen1003 <32509346+naveen1003@users.noreply.github.com>
2018-05-22 12:24:34 +0530

    Update install_ansible.sh

--- setup_tools/install_ansible.sh
+++ setup_tools/install_ansible.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 python --version
-echo "apt-get -qq update"
-#sudo apt-get -qq update
-echo "apt-get -qq -y install python-dev libssl-dev libffi-dev"
-#sudo apt-get -qq -y install python-dev libssl-dev libffi-dev
-echo "pip install pycrypto pyyaml ansible --quiet"
-#sudo pip install pycrypto pyyaml ansible --quiet
-
-echo "apt-get -qq clean"
-#sudo apt-get -qq clean
+ansible --version
+brew install ansible
+ansible-playbook/.playbook.yml
